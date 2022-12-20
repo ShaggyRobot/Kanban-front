aa66d0b03fb81da89ae26b030a32033d45769368
WTFi4 <wtfi4@pop-os.localdomain>
2022-12-20 10:37:34 +0300

    refactor

--- kanban-front/src/CONSTS/endpoints.ts
+++
@@ -1,8 +0,0 @@
-export enum endpoints {
-  baseUrl = 'https://kanban-rest-x8p1.onrender.com',
-  users = 'https://kanban-rest-x8p1.onrender.com/users',
-  signup = 'https://kanban-rest-x8p1.onrender.com/signup',
-  signin = 'https://kanban-rest-x8p1.onrender.com/signin',
-  boards = 'https://kanban-rest-x8p1.onrender.com/boards',
-  file = 'https://kanban-rest-x8p1.onrender.com/file',
-}
--- kanban-front/src/Rtk/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './api/authApi';
-export * from './api/boardsApi';
-export * from './api/usersApi';
-export * from './store';
-export * from './api/types';
